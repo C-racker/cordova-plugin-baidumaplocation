d96c719b78eef481a240f76641cf0e360a39e25d
C-racker <lywp8023@163.com>
2019-11-28 09:38:02 +0800

    修复ios获取不到addr字段和坐标偏移的问题

--- src/ios/BaiduMapLocation.mm
+++ src/ios/BaiduMapLocation.mm
@@ -13,10 +13,10 @@
 {
     NSDictionary *plistDic = [[NSBundle mainBundle] infoDictionary];
     NSString* IOS_KEY = [[plistDic objectForKey:@"BaiduMapLocation"] objectForKey:@"IOS_KEY"];
-    
+
     [[BMKLocationAuth sharedInstance] checkPermisionWithKey:IOS_KEY authDelegate:nil];
-    
-    
+
+
     _localManager = [[BMKLocationManager alloc] init];
     _localManager.delegate = self;
 }
@@ -33,26 +33,47 @@
     if(_execCommand != nil)
     {
         NSMutableDictionary* _data = [[NSMutableDictionary alloc] init];
-        
+
         if(error){
             NSLog(@"locError:{%ld - %@};", (long)error.code, error.localizedDescription);
             NSNumber* errorCode = [NSNumber numberWithInteger: error.code];
             NSString* errorDesc = error.localizedDescription;
-            
+
             [_data setValue:errorCode forKey:@"errorCode"];
             [_data setValue:errorDesc forKey:@"errorDesc"];
         }if(userLocation){
             if(userLocation.location){
+                NSLog(@"location = %@", userLocation.location);
                 NSDate* time = userLocation.location.timestamp;
-                NSNumber* latitude = [NSNumber numberWithDouble:userLocation.location.coordinate.latitude];
-                NSNumber* longitude = [NSNumber numberWithDouble:userLocation.location.coordinate.longitude];
+                CLLocationDegrees latitude = userLocation.location.coordinate.latitude;
+                CLLocationDegrees longitude = userLocation.location.coordinate.longitude;
                 NSNumber* radius = [NSNumber numberWithDouble:userLocation.location.horizontalAccuracy];
-                
+
+                //设置一个目标经纬度
+                CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude,longitude);
+                BMKLocationCoordinateType srctype = BMKLocationCoordinateTypeGCJ02;
+                BMKLocationCoordinateType destype = BMKLocationCoordinateTypeBMK09LL;
+
+                //返回目标坐标系坐标
+                /**
+                 *  @brief 转换为目标经纬度的坐标
+                 *  @param coordinate  待转换的经纬度
+                 *  @param srctype  待转换坐标系类型
+                 *  @param destype  目标坐标系类型
+                 *  @return 目标坐标系经纬度
+                 */
+                CLLocationCoordinate2D cood=[BMKLocationManager BMKLocationCoordinateConvert:coordinate SrcType:srctype DesType:destype];
+
+                NSLog(@"cood = %f,%f", cood.latitude,cood.longitude);
+
+                NSNumber* latitude2 = [NSNumber numberWithDouble:cood.latitude];
+                NSNumber* longitude2 = [NSNumber numberWithDouble:cood.longitude];
+
                 NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
                 [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
                 [_data setValue:[dateFormatter stringFromDate:time] forKey:@"time"];
-                [_data setValue:latitude forKey:@"latitude"];
-                [_data setValue:longitude forKey:@"longitude"];
+                [_data setValue:latitude2 forKey:@"latitude"];
+                [_data setValue:longitude2 forKey:@"longitude"];
                 [_data setValue:radius forKey:@"radius"];
             }
             if(userLocation.rgcData){
@@ -66,7 +87,15 @@
                 NSString* locationDescribe = userLocation.rgcData.locationDescribe;
                 NSString* streetNumber = userLocation.rgcData.streetNumber;
                 NSString* adCode = userLocation.rgcData.adCode;
-                
+                NSArray* poiList = userLocation.rgcData.poiList;
+
+                if (poiList) {
+                    for (BMKLocationPoi * poi in poiList) {
+                        [_data setValue:poi.addr forKey:@"addr"];
+//                         NSLog(@"poi = %@, %@, %f, %@, %@", poi.name, poi.addr, poi.relaiability, poi.tags, poi.uid);
+                    }
+                }
+
                 [_data setValue:countryCode forKey:@"countryCode"];
                 [_data setValue:country forKey:@"country"];
                 [_data setValue:cityCode forKey:@"citycode"];
@@ -79,11 +108,12 @@
                 [_data setValue:locationDescribe forKey:@"locationDescribe"];
             }
         }
-        
+
+
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:_data];
         [result setKeepCallbackAsBool:TRUE];
         [self.commandDelegate sendPluginResult:result callbackId:_execCommand.callbackId];
-        
+
         [_localManager stopUpdatingLocation];
         _execCommand = nil;
     }
